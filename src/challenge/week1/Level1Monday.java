e1b3abbf014b16f0839860d6256de3c5e59ad159
Marco Soares <animemaker008@gmail.com>
2019-11-11 23:16:34 -0600

    Got a closer/more precise result

--- src/challenge/week1/Level1Monday.java
+++ src/challenge/week1/Level1Monday.java
@@ -7,23 +7,55 @@ public class Level1Monday
 {
 
     public static void main(String[] args) {
+        // Creates a user input Scanner
         Scanner input = new Scanner(System.in);     
+        
+        // Scans user input into variable of type String line
         String line = input.nextLine();
+        
+        // Parse String line to Integer line
         int totalCases = Integer.parseInt(line);
+        
+        // Makes an array out of the totalCases
         String[] outputs = new String[totalCases];
+ 
+        // Iterates through the array to determine number of test cases
         for (int testCase = 1; testCase <= totalCases; testCase++) {
+            
+            // Scans the total number of test cases line
             line = input.nextLine();
+            
+            // Splits the values into an array according to spaces
             String[] values = line.split("[ ]");
+            
+            // Gets the indexes of the array to store the different values
             double tP = (double)Integer.parseInt(values[0]);
             double tR = (double)Integer.parseInt(values[1]);
             double k  = Double.parseDouble(values[2]);
             double q  = (double)Integer.parseInt(values[3]);
             double m  = (double)Integer.parseInt(values[4]);
-            double temperature = Math.abs(k * (tP - (tR + (m * q))));
-            outputs[testCase-1] = "Case #" + testCase + " " + temperature;
+            
+            // Processes math
+            double temperature = m * q;
+            
+             double rate = calculateRate(m, tP, k, tR);
+            
+            // Format for the outputs defined
+            outputs[testCase-1] = "Case #" + testCase + " " + Math.abs(temperature - rate);
         }
-        for (int i = 0; i < outputs.length; i++) {
-            System.out.println(outputs[i]);
+        
+        // Outputs final results
+        for (String output : outputs) {
+            System.out.println(output);
+        }
+    }
+    public static double calculateRate(double n, double tP, double k, double tR) {
+        if(n < 1) {
+            double result = 0;
+            return result;
+        } else {
+            double result = (tP - (k * (tP - tR)));
+            return result;
         }
     }
         
